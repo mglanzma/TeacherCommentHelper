46cf81cbf0cabd11f7de5b11d29c7a48b0d3f29e
Michael G <129551069+mglanzma@users.noreply.github.com>
2024-12-14 00:03:03 -0500

    Functioning Instructions Window

--- CommentHelper.py
+++ CommentHelper.py
@@ -67,7 +67,7 @@ def generateStudentComment(commentInput, studentIndex, headers, studentData):
 
 '''
 
-Setting up GUI
+Setting up Primary GUI
 
 '''
 
@@ -80,10 +80,11 @@ def create_window(theme):
 
     menu_layout = [['File',['---','Exit']],
                    ['Open',['Open Student Data','Open Comment Template']],
-                   ['Theme',['Light','Gray','Dark','Dark Fancy']]]
+                   ['Theme',['Light','Gray','Dark','Dark Fancy']],
+                   ['Instructions',['View Instructions']]]
 
-    column_L = [[sg.Multiline(size=(20,20), key='-COMMENT_INPUT-', enable_events=True)],
-                [sg.Multiline(size=(20,20), key='-COMMENT_OUTPUT-')]]
+    column_L = [[sg.Multiline(default_text='Go to "Instructions" for help or begin loading in your files in the correct format', size=(50,20), key='-COMMENT_INPUT-', enable_events=True)],
+                [sg.Multiline(size=(50,20), key='-COMMENT_OUTPUT-')]]
 
     column_R = [[sg.Text('Current Student: '), sg.Text('Open -> Open Student Data', key='-CurrStudent_Text-')],
                  [sg.Button('Prev', key='-Prev_Student-', visible=False), sg.Button('Next', key='-Next_Student-', visible=False)]]
@@ -94,7 +95,87 @@ def create_window(theme):
     return sg.Window('Comment Helper', layout, resizable=True)
 
 
+'''
 
+Setting up Instructions Window
+
+'''
+def create_instructions(currentTheme):
+    sg.theme(currentTheme)
+
+    # text for Instructions textbox
+    instructions = """
+        Instructions:
+
+        NOTES: 
+        - While Instructions Window is open, Comment Helper Window will be unable to process inputs
+        - Change Theme before loading data to avoid bugs
+
+
+1. Go to "Open" in menu
+
+2. In "Open" go to "Open Student Data" and open your .csv file containing your column names and rows of student data
+\t- To get a .csv file, download your Google Sheets spreadsheet as .csv or Comma Separated Values file
+
+3. Once you have added the data, go back to "Open" and click "Open Comment Template"
+\t- Your comment templates should have normal text with "{var}" to use student data. Substitue "var" with the name of the column you want to include data from.
+\t- Optional comment lines should be included after your primary comment and a "-----" separator to indicate they are optional lines. You may have up to 5 optional comment lines in addition to the main comment.
+
+4. Begin copying generic comments from the generic output
+\t- If the optional line checkbox has not been checked, the optional line will be excluded from the output comment
+        
+\nEXAMPLE FILES INCLUDED IN OTHER TABS OF THIS WINDOW
+    """
+
+    # text for CSV File Example
+    csvExample = """First,Last,SingularPronoun,PossesivePronoun,Grade,Year,Extra
+Johnny,Smith,he,his,B+,2024,"without a doubt one of my top students."
+Annie,Doe,she,her,A,2022,"a super hard worker."
+Franklin,McDonald,he,his,A-,2023,"always smiling."
+    """
+
+    # text for Comment File Example
+    commentsExample = """{First} {Last} got a {Grade} in my class this year. I had a fantastic time teaching {First}, and I love {PossesivePronoun} attitude!
+
+{First} is {Extra}
+-----
+This is the Optional Comment 1, but still includes {First}'s name!
+-----
+This is Optional Comment 2. The divider between optional comments is five hyphens.
+-----
+Remember, you can only have one main comment followed by 5 optional comments
+-----
+This is optional comment 4. There is no divider after your last optional comment!
+    """
+
+    # Creating tabs to use in layout
+    instructionsTab = [[sg.Multiline(default_text=instructions, size = (80,40), disabled=True)]]
+
+    csvExampleTab = [[sg.Multiline(default_text=csvExample, size = (80,40), disabled=True)]]
+
+    commentsExampleTab = [[sg.Multiline(default_text=commentsExample, size = (80,40), disabled=True)]]
+
+    # Actual layout incorporating tabs
+    layout = [[sg.TabGroup([[sg.Tab("Instructions", instructionsTab), sg.Tab("CSV File", csvExampleTab), sg.Tab("Comment File", commentsExampleTab)]])]]
+
+    instructionsWindow = sg.Window('Comment Helper Instructions', layout)
+
+    while True:
+        event, values = instructionsWindow.read()
+
+        if event == sg.WIN_CLOSED:
+            break
+
+    instructionsWindow.close()
+
+
+
+
+'''
+
+Main method (loop that keeps window active until closed)
+
+'''
 # creating an instance of our gui window
 window = create_window('GrayGrayGray')
 
@@ -105,13 +186,15 @@ currentStudentIndex = 0
 fullCommentTemplate = []
 commentInput = ""
 
+# theme menu labels to cut down on extra cases
 myThemes = ['Light','Gray','Dark','Dark Fancy']
+currentTheme = "GrayGrayGray"
 
 while True:
 
     # for event tracking
     event, values = window.read()
-        
+
     # Close Window Conditions
     if event == sg.WIN_CLOSED:
         break
@@ -123,13 +206,21 @@ while True:
         window.close()
         match event:
             case 'Light':
+                currentTheme = "GrayGrayGray"
                 window = create_window('GrayGrayGray')
             case 'Gray':
+                currentTheme = "DarkGrey13"
                 window = create_window('DarkGrey13')
             case 'Dark':
+                currentTheme = "Black"
                 window = create_window('Black')
             case 'Dark Fancy':
+                currentTheme = "DarkGrey15"
                 window = create_window('DarkGrey15')
+
+    # Viewing Instructions Window
+    if event == "View Instructions":
+        create_instructions(currentTheme)
 
 
     # Open File Conditions
